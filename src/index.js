887dca57620205c927141aba4913d88149b89c80
Natali258 <natalya.holyavko@gmail.com>
2024-02-25 21:36:37 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { App } from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react-followers">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
